45518697b7a639087eb201eae2e8a6cf9461e109
lsusi3711@gmail.com <Susilchan>
2023-03-07 12:42:00 +0700

    new file:   app/Controllers/H2.php 	app/Views/h2.php

---
+++ app/Controllers/H2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class H2 extends BaseController
+{
+    // public function index()
+    // {
+    //     return view('welcome_message');
+    // }
+
+    public function h2()
+    {
+        return view('h2');
+    }
+}
