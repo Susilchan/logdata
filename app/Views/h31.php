c180ef3598eceed8cd111f26305480618754acec
lsusi3711@gmail.com <Susilchan>
2023-04-06 16:11:20 +0700

    modified:   app/Views/h31.php

--- app/Views/h31.php
+++ app/Views/h31.php
@@ -392,6 +392,15 @@ $data = json_decode($konten, true);
                 border-radius:50px;
             }
 
+            .bt {
+                border-radius: 50%;
+                border: 0px;
+                background-color: #2bd56b;
+                cursor: pointer;
+                padding-bottom: 5px;
+                margin:0px;
+            }
+
         }
 
         @media screen and (max-width: 300px) {
